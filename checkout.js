fb12aad0166728f8e41e6da2a982b0b194822aaf
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-12-04 09:45:09 -0800

    final updates

--- checkout.js
+++ checkout.js
@@ -5,6 +5,7 @@ const itemsList = document.getElementById("cartItems")
 const checkoutButtonElement = document.getElementById("checkoutButton")
 const cart = document.getElementById("cart")
 const totalElement = document.getElementById("total")
+const taxedTotalElement = document.getElementById("totalWithTax")
 var runningTotal = 0;
 
 //2. Create object to hold the barcode, item name, and price
@@ -81,12 +82,31 @@ const inventory = {
         let itemInfo = inventory[barcodeInput]
         let numOfItems = quantityInput;
 
-        let tempPrice = (numOfItems) * parseFloat(itemInfo.price);
-
-        // let tempQuantity = (numOfItems) * parseFloat(itemInfo.quantityInput);
+        let tempPrice = (numOfItems) * parseFloat(itemInfo.price)
 
         //This is calling the function updateTotal to add up the expenses as things get added to the cart
-        updateTotal(tempPrice);
+        checkIfInCart(itemInfo);
+        let foundItem = checkIfInCart(itemInfo);
+
+        //Once the code finds the item that was already scanned and in the cart, it will find the old quantity and change it with a new number that added the old with the new/updated amount
+        if(foundItem){
+            let oldQuantity = foundItem.querySelector(".quantityOfItem").innerText
+
+            //This sets the new and old quantities as integers
+            let newQuantity = parseInt(oldQuantity) + parseInt(numOfItems);
+
+            //This changes the text from the old quantity with the new one
+            foundItem.querySelector(".quantityOfItem").innerText = newQuantity;
+
+            //This will update the price with the updated quantity
+            updateTotal(tempPrice);
+
+            //This returns the new information
+            return;
+        }
+            //This calls the function updateTotal and calculates the total of the current items in cart without tax
+            updateTotal(tempPrice);
+        
         
         //This creates a div for the item that will appear on the cart 
         const itemElement = document.createElement("div")
@@ -95,15 +115,18 @@ const inventory = {
 
         //This creates a p element so that the name of the item can appear on the shopping cart list
         const itemLabel = document.createElement("p");
+        itemLabel.classList.add("nameOfItem");
         
         //This changes the text to the item's name on the list of things in the cart
         itemLabel.innerText = itemInfo.itemName;
         //This creates a p element for the price to appear
         const itemPrice = document.createElement("p");
+        itemPrice.classList.add("priceOfItem");
         //This changes the text to the item's price
         itemPrice.innerText = itemInfo.price;
         //This creates a p element for the quantity to appear 
         const itemQuantity = document.createElement("p");
+        itemQuantity.classList.add("quantityOfItem");
         //This changes the text to the amount of items in the cart
         itemQuantity.innerText = numOfItems
 
@@ -115,9 +138,22 @@ const inventory = {
         //This makes the itemElement(price, label, quantitiy) a child of the cart 
         cart.appendChild(itemElement);
     }
+ }
 
-    
-    
+ //This function will check through all the items that has been scanned and in the cart
+ function checkIfInCart(itemInfo){
+    //This variable grabs all the info in the items class
+    let itemsInCart = document.querySelectorAll(".items");
+
+    //This for loop will go through the array itemsInCart to find and match the newly scanned item with things that are already in the cart
+    for(let i = 0; i < itemsInCart.length; i++){
+        let nameOfItem = itemsInCart [i].querySelector(".nameOfItem")
+        
+        if(itemInfo.itemName === nameOfItem.innerText)
+        
+        //If found, it will return the info of the item that's in the cart
+        return itemsInCart[i];
+    }
  }
 
  //This function will update the price as things get added to the cart
@@ -128,15 +164,19 @@ const inventory = {
     totalElement.innerText = "Total: $" + runningTotal;
  }
 
- //This function is supposed to update the quantity as the same things are being scanned multiple times and won't appear twice
-//  function updateQuantity (){
-//     for(let i = 0; i < cartItems.length; i++){
-//         const itemelement = cartItems [i];
-//         const barcode = itemElement.getAttribute("barcode")
-//         const quantitiy = itemElement.getAttribute("barcode")
+ //This function is supposed to calculate the total including sales tax 
+ function checkoutCart() {
+    let salesTax = 0.0925;
+    let finalTotal = runningTotal + runningTotal * salesTax;
 
-//     }
-//  }
- 
- //When the user clicks the add to cart button, the barcode that got scanned will be searched in the code
- document.getElementById('addCartButton').addEventListener('click', addToCart);
+    // Use the correct variable name here: taxedTotalElement
+    taxedTotalElement.innerText = "Your grand total (including tax, 9.25%) is: $" + finalTotal;
+
+    runningTotal = 0;
+}
+
+//When the user clicks the checkoutButton, the total without tax will be calculated with the sales tax for the final total
+document.getElementById("checkoutButton").addEventListener("click", checkoutCart);
+
+// When the user clicks the add to cart button, the barcode that got scanned will be searched in the code
+document.getElementById('addCartButton').addEventListener('click', addToCart);
